df35879e130d201cc84af1afb61edd736e286de5
Abhishek Prajapati <114478676+abhiperfect@users.noreply.github.com>
2023-09-08 19:00:20 +0530

    New Project Country and its capital
    class project

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction( val ){
+    if( val === "india")
+        document.querySelector("#capital").textContent = "Delhi"; 
+    if( val === "china")
+        document.querySelector("#capital").textContent = "Beijing"; 
+    if( val === "japan")
+        document.querySelector("#capital").textContent = "Tokyo"; 
+    if( val === "russia")
+        document.querySelector("#capital").textContent = "Moscow"; 
+    if( val === "ukrain")
+        document.querySelector("#capital").textContent = "Kyiv"; 
+    
+}
